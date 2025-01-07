4c050127518424f6600e8248ea37c302828f8c82
pnagi007 <162715712+pnagi007@users.noreply.github.com>
2025-01-07 15:01:59 +0530

    Add files via upload

---
+++ IfElseExample.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class IfElseExample
+{
+public static void main(String[] args)
+{
+int number = 20;
+if(number>10)
+{
+System.out.println("The no is greater than 10");
+}
+else
+{
+System.out.println("The no is greater than or equal to 10");
+}
+}
+}
