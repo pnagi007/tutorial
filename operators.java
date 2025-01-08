ff5273cbd5433e58b7953148ada3f914c474927c
Naga Gayathri <162715712+pnagi007@users.noreply.github.com>
2025-01-08 09:47:39 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class operators
+{ 
+public static void main(String[] args)
+{
+int num1=10;
+int num2=90;
+int addition=num1+num2;
+int subtraction=num1-num2;
+int multiplication=num1*num2;
+double division=num1/num2;
+System.out.println("Addition: " + addition);
+System.out.println("Subtraction: " + subtraction);
+System.out.println("Multiplication: " + multiplication);
+System.out.println("Division: " + division);
+}
+}
